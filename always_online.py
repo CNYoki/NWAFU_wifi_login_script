e7e7d14bf8fd37fd4f654ac67cd22b8fed50f69a
Yoki <i@yokii.cn>
2021-10-23 19:40:05 +0800

    adapt to Internet checking

--- always_online.py
+++ always_online.py
@@ -4,11 +4,12 @@ from NWAFU_WIFI_login.LoginManager import LoginManager
 
 
 def is_connect_internet(testip):
-    status = os.system(u"ping {} -c 8".format(testip))
+    status = os.system(u"ping {} -w 8".format(testip))
     return status == 0
 
 
-def always_login(username, password, testip, checkinterval):
+def always_login(username, password, checkinterval):
+    testip = "210.27.84.131"  # 只能由内网访问
     lm = LoginManager()
     login = lambda: lm.login(username=username, password=password)
     timestamp = lambda: print(time.asctime(time.localtime(time.time())))
@@ -21,6 +22,7 @@ def always_login(username, password, testip, checkinterval):
     while 1:
         time.sleep(checkinterval)
         if not is_connect_internet(testip):
+            print("失败")
             timestamp()
             try:
                 login()
@@ -31,7 +33,6 @@ def always_login(username, password, testip, checkinterval):
 if __name__ == "__main__":
     username = "nwafu_account"
     password = "nwafu_password"
-    testip = "nwafu.edu.cn"  # IP to test whether the Internet is connected
-    checkinterval = 5 * 60
+    checkinterval = 5 * 60  # 每五分钟检测一次
 
-    always_login(username, password, testip, checkinterval)
+    always_login(username, password, checkinterval)
